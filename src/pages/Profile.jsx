10c656a2d91350721912b35724cd5a570c8452ae
Bananasuper-coding <nttgm679@gmail.com>
2026-05-13 00:23:39 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useUserStore } from '../stores/userStore';
+import { useUserStore } from '../stores/lessonStore';
 import { Card, Button, Badge, ProgressBar, Avatar } from '../components/ui';
 import { Link } from 'react-router-dom';
 
